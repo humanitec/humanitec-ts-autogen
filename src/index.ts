6eaed1e7d57dce1f6f5cc242a2e3002343ede2b4
Johannes Würbach <johannes.wuerbach@googlemail.com>
2024-06-21 08:47:30 +0200

    feat: generate using typescript-fetch

--- src/index.ts
+++ src/index.ts
@@ -1,48 +1,49 @@
-import { Configuration } from './generated/configuration';
+import { Configuration } from "./generated/index.js";
 
-export * from './generated'
+export * from "./generated/index.js";
 
 export interface APIConfig {
-  token: string
-  apiHost?: string
-  internalApp?: string
+  token: string;
+  apiHost?: string;
+  internalApp?: string;
 }
 
-const defaultAPIHost = 'https://api.humanitec.io'
-const sdk = 'humanitec-ts-autogen'
-const sdkVersion = 'latest'
+const defaultAPIHost = "https://api.humanitec.io";
+const sdk = "humanitec-ts-autogen";
+const sdkVersion = "latest";
 
 interface UserAgentDetails {
-  sdk: string
-  app?: string
+  sdk: string;
+  app?: string;
 }
 
 const humanitecUserAgentHeader = ({ sdk, app }: UserAgentDetails): string => {
-  const elements = []
+  const elements = [];
   if (sdk) {
-    elements.push(`sdk ${sdk}`)
+    elements.push(`sdk ${sdk}`);
   }
   if (app) {
-    elements.push(`app ${app}`)
+    elements.push(`app ${app}`);
   }
 
-  return elements.join('; ')
-}
+  return elements.join("; ");
+};
 
 export const apiConfig = (config: APIConfig): Configuration => {
   if (!config.token) {
-    throw new Error('token needs to provided')
+    throw new Error("token needs to provided");
   }
 
   const apiConfig = new Configuration({
     basePath: config.apiHost || defaultAPIHost,
-    baseOptions: {
-      headers: {
-        'Authorization': `Bearer ${config.token}`,
-        'Humanitec-User-Agent': humanitecUserAgentHeader({ app: config.internalApp, sdk: `${sdk}/${sdkVersion}` })
-      },
+    headers: {
+      Authorization: `Bearer ${config.token}`,
+      "Humanitec-User-Agent": humanitecUserAgentHeader({
+        app: config.internalApp,
+        sdk: `${sdk}/${sdkVersion}`,
+      }),
     },
   });
 
   return apiConfig;
-}
+};
